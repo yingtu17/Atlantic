4af1f480be8c197f868def4b5e832617edb5c446
yingtu17 <yingtuboss@gmail.com>
2023-01-22 01:21:36 +0600

    changes

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -4,10 +4,10 @@
 			<img class="w-full h-full" src="../assets/Cover.avif" alt="" />
 		</div>
 		<div class="w-full flex justify-center items-center text-[#fceacd] bg-[#005b55]">
-			<div class="w-4/5 p-6">
-				<div class="w-full"><h1 class="text-center font-mono text-2xl">HOURS</h1></div>
+			<div class="w-4/5 p-6 whitespace-nowrap">
+				<div class="w-full"><h1 class="text-center font-mono text-2xl">{{ $t("footer.hours") }}</h1></div>
 				<div class="flex py-2 justify-between items-center gap-4">
-					<p>MON</p>
+					<p>{{ $t("footer.monday") }}</p>
 					<div class="w-full border-dashed border-[1px] border-[#fceacd]"></div>
 					<p class="whitespace-nowrap">10:00 am - 1:00 am</p>
 				</div>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,7 +4,7 @@
 			<span @click="handleClick3">
 				<label><nut-icon name="horizontal-n"></nut-icon></label>
 			</span>
-			<span class=""> Atlantic</span>
+			<span class="text-xl"> Atlantic</span>
 			<span class="flex items-center gap-4">
 				<LanSwitching />
 				<AudioVue />
@@ -12,24 +12,7 @@
 		</div>
 	</nut-cell>
 	<nut-popup position="left" v-model:visible="show3" :style="{ width, height }">
-		<nut-sidenavbar :show="show3">
-			<nut-subsidenavbar title="图像理解" ikey="3" :open="false">
-				<nut-sidenavbaritem ikey="4" title="菜品识别"></nut-sidenavbaritem>
-				<nut-sidenavbaritem ikey="5" title="拍照购"></nut-sidenavbaritem>
-			</nut-subsidenavbar>
-			<nut-subsidenavbar title="智能城市AI" ikey="6">
-				<nut-sidenavbaritem ikey="7" title="企业风险预警模型"></nut-sidenavbaritem>
-				<nut-sidenavbaritem ikey="8" title="水质量检测"></nut-sidenavbaritem>
-				<nut-subsidenavbar title="人体识别" ikey="9">
-					<nut-sidenavbaritem ikey="10" title="人体检测"></nut-sidenavbaritem>
-					<nut-sidenavbaritem ikey="11" title="细粒度人像分割"></nut-sidenavbaritem>
-				</nut-subsidenavbar>
-			</nut-subsidenavbar>
-			<nut-subsidenavbar title="自然语言处理" ikey="12">
-				<nut-sidenavbaritem ikey="13" title="词法分析"></nut-sidenavbaritem>
-				<nut-sidenavbaritem ikey="14" title="句法分析"></nut-sidenavbaritem>
-			</nut-subsidenavbar>
-		</nut-sidenavbar>
+		112423
 	</nut-popup>
 </template>
 <script>
@@ -52,20 +35,6 @@
 
 			const handleClick3 = () => {
 				state.show3 = true;
-				setTimeout(() => {
-					state.navs = [
-						{
-							id: 16,
-							name: "asyc abc16",
-							arr: [{ pid: 16, id: 17, name: "abc16-id17" }],
-						},
-						{
-							id: 17,
-							name: "asyc abc17",
-							arr: [{ pid: 17, id: 18, name: "abc17-id18" }],
-						},
-					];
-				}, 2000);
 			};
 
 			const handleClick4 = (msg) => {};
